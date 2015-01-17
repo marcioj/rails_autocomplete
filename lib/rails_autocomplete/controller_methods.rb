4345473b19462350cd66ce5bd1fd5b2a1da125b8
Marcio Junior <marciojunior_eu@yahoo.com.br>
2015-01-17 17:50:32 -0200

    Changed generated autocomplete method to use jquery ui autocomplete json format

--- lib/rails_autocomplete/controller_methods.rb
+++ lib/rails_autocomplete/controller_methods.rb
@@ -10,12 +10,19 @@ module RailsAutocomplete
       def autocomplete(field, options = {})
         model_name = controller_name.gsub(/_controller$/, "").singularize
         options[:controller_name] = controller_name
+        options[:field] = field
         self.autocomplete_fields ||= {}
         self.autocomplete_fields[model_name.to_sym] = options
 
-        define_method "autocomplete_#{model_name}" do
+        define_method "autocomplete_#{field}" do
           model_class = model_name.classify.constantize
-          render json: model_class.select(field)
+          relation = model_class.where("lower(#{field}) LIKE ?", "#{params[:term]}%").select(:id, field)
+          attributes = relation.to_a.map(&:attributes)
+          attributes.each do |attribute|
+            attribute["value"] = attribute.delete(field.to_s)
+            attribute["label"] = attribute["value"]
+          end
+          render json: attributes
         end
       end
     end
--- spec/dummy/spec/controllers/users_controller_spec.rb
+++ spec/dummy/spec/controllers/users_controller_spec.rb
@@ -5,17 +5,24 @@ RSpec.describe UsersController, type: :controller do
     JSON.parse(response.body)
   end
 
-  describe "GET autocomplete_user" do
-    let!(:user_1) { create(:user, name: "foo") }
-    let!(:user_2) { create(:user, name: "bar") }
-    let!(:user_3) { create(:user, name: "yo") }
+  describe "GET autocomplete_name" do
+    let(:term) { "" }
+    let!(:user_1) { create(:user, name: "marcio") }
+    let!(:user_2) { create(:user, name: "rodrigues") }
+    let!(:user_3) { create(:user, name: "correa") }
+    let!(:user_4) { create(:user, name: "marcio junior") }
 
     def action
-      get :autocomplete_user, format: :json
+      get :autocomplete_name, term: term, format: :json
     end
 
-    before { action }
+    context "simple search" do
+      let(:term) { "mar" }
+      before { action }
 
-    it { expect(json_response.map { |r| r["name"] }).to match_array(["foo", "bar", "yo"]) }
+      it { expect(json_response.map { |r| r["id"] }).to match_array([user_1.id, user_4.id]) }
+      it { expect(json_response.map { |r| r["label"] }).to match_array(["marcio", "marcio junior"]) }
+      it { expect(json_response.map { |r| r["value"] }).to match_array(["marcio", "marcio junior"]) }
+    end
   end
 end
